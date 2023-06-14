113ff92cebb0f7f132c5385a1b2d1a06598ded76
Jirashy <divinetrees@gmail.com>
2023-06-15 01:42:54 +0700

    Fix casting string to int cause int has limit size

--- answer2/answer2.go
+++ answer2/answer2.go
@@ -3,6 +3,12 @@ package answer2
 import "strconv"
 
 func ContainsOnlyDigits(str string) bool {
-	_, err := strconv.Atoi(str)
-	return err == nil
+	for _, c := range str {
+		_, err := strconv.Atoi(string(c))
+		if err != nil {
+			return false
+		}
+	}
+
+	return true
 }
